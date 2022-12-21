80914c117abe668c42a97c65d7aa65dc08d2cb20
kimminsu <xjfcnfw3@naver.com>
2022-12-21 12:39:54 +0900

    feat: 2장 색을 파라미터화해서 필터링

---
+++ src/main/java/chap2/ColorApple.java
@@ -0,0 +1,16 @@
+package chap2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorApple {
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, String color) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor().equals(color)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+}
