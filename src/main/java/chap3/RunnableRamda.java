49db0937584dc08fedd74c74e55382fcb4ec810a
kimminsu <xjfcnfw3@naver.com>
2022-12-21 15:51:04 +0900

    feat: 3장 함수형 인터페이스 람다 적용

---
+++ src/main/java/chap3/RunnableRamda.java
@@ -0,0 +1,22 @@
+package chap3;
+
+public class RunnableRamda {
+
+
+    public static void process(Runnable r) {
+        r.run();
+    }
+
+    public static void main(String[] args) {
+        Runnable r1 = () -> System.out.println("Hello World");
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello World2");
+            }
+        };
+        process(r1);
+        process(r2);
+        process(() -> System.out.println("Hello World 3"));
+    }
+}
