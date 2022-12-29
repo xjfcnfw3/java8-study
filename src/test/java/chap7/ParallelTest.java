7a6c60b1b7894981dd72cff759e0841733c95b30
kimminsu <xjfcnfw3@naver.com>
2022-12-29 13:39:32 +0900

    feat: 7장 스트림 성능 측정

--- src/test/java/chap7/ParallelTest.java
+++ src/test/java/chap7/ParallelTest.java
@@ -29,6 +29,44 @@ public class ParallelTest {
     }
 
 
+    @Test
+    void measureSequentialSum() {
+        System.out.println("Sequential sum done in: " +
+            measureSumPerf(ParallelTest::sequentialSum, 10_000_000) + " msecs");
+    }
+
+    @Test
+    void measureIterativeSum() {
+        System.out.println("Iterative sum done in: " +
+            measureSumPerf(ParallelTest::iterativeSum, 10_000_000) + " msecs");
+    }
+
+    @Test
+    void measureParallelSum() {
+        System.out.println("Parallel sum done in: " +
+            measureSumPerf(ParallelTest::parallelSum, 10_000_000) + " msecs");
+    }
+
+    public static long iterativeSum(long n) {
+        long result = 0;
+        for (long i = 0; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    public long measureSumPerf(Function<Long, Long> adder, long n) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            long sum = adder.apply(n);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            System.out.println("Result: " + sum);
+            if (duration < fastest) fastest = duration;
+        }
+        return fastest;
+    }
+
     public static long sequentialSum(long n) {
         return Stream.iterate(1L, i -> i + 1)
             .limit(n)
