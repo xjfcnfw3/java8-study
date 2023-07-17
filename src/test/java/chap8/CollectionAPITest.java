efc4b6500ca96b4a9da023ff80491304913d7939
kimminsu <xjfcnfw3@naver.com>
2023-07-17 19:14:53 +0900

    8장 UnsupportedOperationException 예시 추가

---
+++ src/test/java/chap8/CollectionAPITest.java
@@ -0,0 +1,20 @@
+package chap8;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CollectionAPITest {
+
+    @Test
+    void unSupportedOperationExceptionTest() {
+        List<String> cars = Arrays.asList("bus", "mini", "sport");
+
+        assertThatThrownBy(() -> cars.add("truck"))
+            .isInstanceOf(UnsupportedOperationException.class);
+
+    }
+
+}
